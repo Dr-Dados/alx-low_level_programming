a221e633ee2828940f772dbd538788e3eaaa91dd
Dr-Dados <adnane.bachchar@gmail.com>
2023-04-17 10:00:52 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog contains dog's basic info
+ *@name: first element
+ *@age :second element
+ *owner :third element
+ *Description : Define a new type struct dog with three elements
+ */
+
+struct dog
+{
+  char *name;
+  int age;
+  char *owner;
+}
+
+#endif
