2277deb04f5d2010773d416c85baa63ce783c23d
Dr-Dados <adnane.bachchar@gmail.com>
2023-04-20 03:56:39 -0700

    1. To be is to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,8 +21,8 @@ for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(args, int));
  if (i != (n - 1) && separator != NULL)
-printf("%s", *separator);
+printf("%s", separator);
 }
 printf("\n");
-va_end(args):
+ va_end(args);
 }
