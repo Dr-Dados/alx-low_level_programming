a56b2550fac2243d9310583c1a508a78736bc149
Dr-Dados <adnane.bachchar@gmail.com>
2023-05-08 07:02:12 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *buffer;
+ssize_t fp;
+ssize_t a;
+ssize_t b;
+
+fp = open(filename, O_RDONLY);
+if (fp == -1)
+return (0);
+buffer = malloc(sizeof(char)*letters);
+b = read(fp,buffer,letters);
+a = write(STDOUT_FILENO, buffer, b);
+free(buffer);
+close(fp);
+return (a);
+}
